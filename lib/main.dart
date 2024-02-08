3339e57932128e33bdd91189ed93962fb58459b8
samuel <samueloguobi@gmail.com>
2024-02-08 02:20:15 +0100

    adding the first three rows of the project

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,52 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
             body: Container(
-          child: // appBar: AppBar(
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Image.asset('assets/arrow-left.jpg'), // First image
-            const Text(
-              'Details',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'Sora',
-                  fontWeight: FontWeight.bold),
-            ), 
-            Image.asset('assets/Heart.jpg'), //another comment Second image
-          ]),
-        )));
+                child: Column(children: [
+          // appBar: AppBar(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/arrow-left.jpg'), // First image
+                  const Text(
+                    'Detail',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Sora',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Image.asset(
+                      'assets/Heart.jpg'), //another comment Second image
+                ]),
+          ),
+          Image.asset("assets/Rectangle.jpg"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 155.0),
+                child: Text("Cappucino",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Sora',
+                        fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 155.0),
+                child: Text("with Chocolate",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                        fontFamily: 'Sora',
+                        fontWeight: FontWeight.normal)),
+              ),
+            ],
+          )
+        ]))));
   }
 }
