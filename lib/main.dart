c36b127ce5749c3ba507261627816f286b121762
samuel <samueloguobi@gmail.com>
2024-02-08 22:08:40 +0100

    added another text

--- lib/main.dart
+++ lib/main.dart
@@ -10,122 +10,132 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          child: Column(
-            children: [
-              Padding(
-                // padding: const EdgeInsets.all(8.0),
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.15),
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset('assets/arrow-left.jpg'), // First image
-                    const Text(
-                      'Detail',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: 'Sora',
-                        fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.1),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        'assets/arrow-left.jpg',
+                        height: 50,
                       ),
-                    ),
-                    Image.asset('assets/Heart.jpg'), // Second image
-                  ],
-                ),
-              ),
-              Image.asset("assets/Rectangle.jpg"),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.15),
-                    child: Text(
-                      "Cappucino",
-                      style: TextStyle(
-                          fontSize: 20.0,
+                      Text(
+                        'Detail',
+                        style: TextStyle(
+                          fontSize: 24,
                           fontFamily: 'Sora',
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.15),
-                    child: Text(
-                      "with Chocolate",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                          fontFamily: 'Sora',
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // First Row with star and text
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.15),
-                    child: Row(
-                      children: [
-                        Icon(Icons.star, color: Colors.yellow),
-                        SizedBox(width: 8.0),
-                        Text(
-                          '4.8',
-                          style: TextStyle(
-                            fontFamily: 'Sora',
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(width: 4.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      Image.asset(
+                        'assets/Heart.jpg',
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.15),
+                    child: Image.asset(
+                      "assets/Rectangle.jpg",
+                      scale: 0.87,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.15),
+                    child: Column(
+                      children: [
                         Text(
-                          '(230)',
+                          "Cappucino",
                           style: TextStyle(
-                            fontFamily: 'Sora',
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.normal,
-                          ),
+                              fontSize: 20.0,
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "with Chocolate",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15.0,
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.normal),
+                          textAlign: TextAlign.center,
                         ),
                       ],
                     ),
                   ),
-                  // Second Row with images
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.15),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          'assets/bean.jpg',
-                          height: 50,
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.yellow),
+                          SizedBox(width: 8.0),
+                          Text(
+                            '4.8',
+                            style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '(230)',
+                            style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/bean.jpg',
+                            height: 50,
+                          ),
+                          SizedBox(width: 8.0),
+                          Image.asset(
+                            'assets/milk.jpg',
+                            height: 50,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.grey[300], // Light grey color
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Description',
+                        style: TextStyle(
+                          fontFamily: 'Sora',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(width: 8.0),
-                        Image.asset(
-                          'assets/milk.jpg',
-                          height: 50,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.15),
-                child: Divider(
-                  color: Colors.grey[300], // Light grey color
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
